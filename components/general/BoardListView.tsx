db6075886fdfd45e6009563a713c37c4b0c1702c
Bryan Salonga <winstonbryan01@gmail.com>
2025-12-28 16:44:46 +0800

    added scroll area

--- components/general/BoardListView.tsx
+++ components/general/BoardListView.tsx
@@ -2,6 +2,7 @@
 import { Layers01Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import BoardItemView from "../kanban/BoardItemView";
+import { ScrollArea } from "../ui/scroll-area";
 
 //STORE
 import { useLocalState } from "@/lib/stores";
@@ -13,30 +14,34 @@ export default function BoardListView() {
   const boardList = useLocalState((state) => state.Boards);
 
   return (
-    <div className="flex flex-col mb-8 h-[450px]">
-      <div className="mb-6 flex items-center justify-between w-full border-b border-dashed pb-2">
-        <div className="flex items-center gap-2">
-          <HugeiconsIcon
-            icon={Layers01Icon}
-            className="size-4 text-muted-foreground"
-          />
-          <p className="text-xs uppercase tracking-wide text-muted-foreground">
-            your boards
-          </p>
+    <div className="flex flex-col mb-8 h-full">
+      <div className="px-4">
+        <div className="mb-6 flex items-center justify-between w-full border-b border-dashed pb-2">
+          <div className="flex items-center gap-2">
+            <HugeiconsIcon
+              icon={Layers01Icon}
+              className="size-4 text-muted-foreground"
+            />
+            <p className="text-xs uppercase tracking-wide text-muted-foreground">
+              your boards
+            </p>
+          </div>
+          <AddBoardDialog />
         </div>
-        <AddBoardDialog />
       </div>
       {boardList.length > 0 ? (
-        <nav className="space-y-2 text-sm">
-          {boardList.map((board) => (
-            <BoardItemView
-              id={board.id}
-              key={board.id}
-              label={board.label}
-              total_columns={board.total_columns}
-            />
-          ))}
-        </nav>
+        <ScrollArea className="h-[600px] px-4">
+          <nav className="space-y-2 text-sm">
+            {boardList.map((board) => (
+              <BoardItemView
+                id={board.id}
+                key={board.id}
+                label={board.label}
+                total_columns={board.total_columns}
+              />
+            ))}
+          </nav>
+        </ScrollArea>
       ) : (
         <EmptyBoardList />
       )}
--- components/kanban/SideBar.tsx
+++ components/kanban/SideBar.tsx
@@ -2,7 +2,7 @@ import BoardListView from "../general/BoardListView";
 
 export default function SideBar() {
   return (
-    <aside className="flex w-[280px] min-w-[260px] flex-col border-r border-dashed bg-white py-6 px-4">
+    <aside className="flex w-[280px] min-w-[260px] flex-col border-r border-dashed py-6 bg-white">
       <BoardListView />
     </aside>
   );
